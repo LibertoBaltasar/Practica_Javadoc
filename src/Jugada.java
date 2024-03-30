725a358ab10c9d85b9a422a27cbeb9922647fc30
Liberto Méndez <liberto.baltasar@gmail.com>
2024-03-30 19:59:24 +0100

    Iniciados los comentarios

--- src/Jugada.java
+++ src/Jugada.java
@@ -7,13 +7,29 @@ public class Jugada {
     static int TurnosHastaTijera;
     de cara a posibles ampliaciones para poder llevar la estadística
      */
+    /**
+     * Gestiona cada una de las jugadas o turnos dentro del juego.
+     * @author Liberto
+     * @version 1.0
+     */
     int jugadaHecha1;
     int jugadaHecha2;
 
+    /**
+     * Constructor de la clase
+     * @see Menu
+     * @param jugadaHecha1 Valor entero que determina que opción ha escagido el jugador1
+     * @param jugadaHecha2 Valor entero que determina que opción ha escagido el jugador2
+     */
     public Jugada(int jugadaHecha1, int jugadaHecha2) {
         this.jugadaHecha1 = jugadaHecha1;
         this.jugadaHecha2 = jugadaHecha2;
     }
+
+    /**
+     *
+     * @return Valor entero que determina quién ha ganado la jugada: 1-->jugador1; 2-->jugador2; 0-->empate
+     */
     public int resolver(){
         if (this.jugadaHecha1==jugadaHecha2){
             return 0;
